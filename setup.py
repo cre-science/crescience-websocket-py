88d68afaa9af6d64d848ac8fbc18d3f15c64202f
haschtl <haschtl@pm.me>
2023-12-07 16:31:22 +0100

    Added requests dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["requests"],
     keywords=['pypi','crescience', 'python'],
     classifiers=[
         "Development Status :: 1 - Planning",
